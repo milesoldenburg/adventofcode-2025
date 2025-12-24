5fd2d75b07b77e96d128fa8164970e57d0f5a3e3
Miles Oldenburg <moldenbu@cisco.com>
2025-12-23 23:47:26 -0500

    Day 10 Part 1

--- 10/part1.py
+++ 10/part1.py
@@ -1,8 +1,11 @@
+from itertools import combinations
 import re
 
-pattern = re.compile(r'\[([\.#]+)\]\s(\(.*\))\s\{(.*)\}')
+pattern = re.compile(r'\[([.#]+)]\s(\(.*\))\s\{(.*)}')
 
-for line in open("test.txt"):
+total_shortest = 0
+
+for line in open("input.txt"):
     match = re.match(pattern, line.strip())
     if match:
         indicator_lights_match = match.group(1)
@@ -10,3 +13,20 @@ for line in open("test.txt"):
 
         buttons_match = match.group(2)
         buttons = [list(map(int, button.strip('()').split(','))) for button in buttons_match.split()]
+        
+        shortest = 0
+        for i in range(len(buttons)):
+            for combo in combinations(buttons, i + 1):
+                lights = 0
+                for button in combo:
+                    for press in button:
+                        lights ^= (1 << len(indicator_lights_match) - 1 - press)
+                if lights == indicator_lights:
+                    shortest = i + 1
+                    break
+            if shortest > 0:
+                break
+        
+        total_shortest += shortest
+
+print(total_shortest)
